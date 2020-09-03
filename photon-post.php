49959bcd25b61ee2238c40235f07a3434109bb4b
i-am-phi <phi@i-am-phi.net>
2020-09-03 10:17:42 -0700

    convert to submodule

---
+++ photon-post.php
@@ -0,0 +1,94 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+use Grav\Common\Grav;
+use Grav\Common\Page\Collection;
+use Grav\Common\Page\Page;
+use Grav\Common\Page\Pages;
+use Grav\Common\Taxonomy;
+
+/**
+ * Class PhotonPostPlugin
+ * @package Grav\Plugin
+ */
+class PhotonPostPlugin extends Plugin
+{
+
+    public static function getSubscribedEvents()
+    {
+      return [
+        'onPluginsInitialized' => ['onPluginsInitialized', 0],
+        'onGetPageTemplates' => ['onGetPageTemplates', 0]
+      ];
+    }
+
+    public function onPluginsInitialized()
+    {
+
+      if ( $this->isAdmin() ) {
+
+        $this->enable([
+          'onAdminSave' => ['onAdminSave', 0], //from events plugin - not sure if necessary
+          // 'onGetPageTemplates' => ['onGetPageTemplates', 0]
+        ]);
+
+        return;
+      }
+
+      $this->enable([
+        'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+        'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+      ]);
+
+      return;
+
+    }
+
+    // called when saving page in admin
+    public function onAdminSave(Event $event)
+    {
+      // placeholder
+    }
+
+
+    /** Add blueprint directories for admin templates.     */
+    public function onGetPageTemplates(Event $event)
+    {
+        $types = $event->types;
+        $locator = Grav::instance()['locator'];
+        $types->scanBlueprints($locator->findResource('plugin://' . $this->name . '/blueprints'));
+        $types->scanTemplates($locator->findResource('plugin://' . $this->name . '/templates'));
+    }
+
+    /**  Add current directory to twig lookup paths     */
+    public function onTwigTemplatePaths()
+    {
+      $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+
+
+    public function onTwigSiteVariables()
+    {
+      // setup
+      $page = 			$this->grav['page'];
+      $pages = 			$this->grav['pages'];
+      $twig = 			$this->grav['twig'];
+      $assets = 		$this->grav['assets'];
+
+      // styles
+      $css = 'plugin://photon-post/assets/post.css';
+      $assets->addCss($css, 100, true, 'photon-plugins' );
+
+      // only load the vars if this datatype page
+      if ($page->template() == 'post')
+      {
+
+        // scripts
+        $js = 'plugin://photon-post/assets/post.js';
+        $assets->addJs($js, 100, true, 'defer', 'photon-plugins' );
+      }
+    }
+}
